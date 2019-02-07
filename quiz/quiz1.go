b86c7f71d089baf803836490ff89c3b3c9b72053
Maxime <mcote074@uottawa.ca>
2019-02-07 14:51:16 -0500

    Update quiz1.go

--- quiz/quiz1.go
+++ quiz/quiz1.go
@@ -1,12 +1,25 @@
+/** CSI 2520 - Quiz 1 - Maxime Côté-Gagné - 8851539 **/
+
 /*
- *CSI 2520 - Quiz 1 - Maxime Côté-Gagné - 8851539
- */
+On dispose d'un tableau comme par exemple le suivant:
+
+var tableau = [7]int{3, 4, 8, 9, 5, 2, 7}
+
+et on voudrait pouvoir trouver les éléments minimum et maximum dans ce tableau et les déplacer au début du tableau.
+Après cette opération, le contenu du tableau sera donc comme suit:
+
+{2, 9, 8, 4, 5, 3, 7}
+
+L'ordre des autres éléments n'a pas d'importance pourvu que le premier élément soit le minimum et le second soit le maximum.
+
+Écrire une fonction Go permettant d'effectuer cette opération. Évidemment, cette fonction doit pouvoir fonctionner pour n'importe quel tableau (ou slice).
+*/
 package main
 
 import "fmt"
 
-func sliceminmax(slice []int) []int{
-	fmt.Println("Original array/slice:",slice)
+func sliceminmax(slice []int) []int {
+	fmt.Println("Original array/slice:", slice)
 
 	//VAR
 	var min int = 0
@@ -16,30 +29,30 @@ func sliceminmax(slice []int) []int{
 	//MIN LOOP
 	//fmt.Println("MIN LOOP")
 	for i, elem := range slice {
-		if i==0 || elem < min {
-			min = elem	
+		if i == 0 || elem < min {
+			min = elem
 			pos = i
 			//fmt.Println("pos:",pos)
 			//fmt.Println("min:",min)
 		}
-		
+
 	}
 	slice[pos], slice[0] = slice[0], min
-	fmt.Println("After min loop:",slice)
+	fmt.Println("After min loop:", slice)
 
 	//MAX LOOP
 	//fmt.Println("MAX LOOP")
 	for i, elem := range slice {
-		if i==0 || elem > max {
+		if i == 0 || elem > max {
 			max = elem
-			pos = i	
+			pos = i
 			//fmt.Println("pos:",pos)
 			//fmt.Println("max:",max)
 		}
 	}
 	slice[pos], slice[1] = slice[1], max
-	fmt.Println("After max loop:",slice)
-	
+	fmt.Println("After max loop:", slice)
+
 	//fmt.Println("Minimum:",min)
 	//fmt.Println("Maximum:",max)
 
@@ -47,12 +60,12 @@ func sliceminmax(slice []int) []int{
 }
 
 func main() {
-	var tableau = [7]int{3, 4, 8, 9, 5, 2, 7}//array
-	
-	var slice []int = tableau[:]//slice
-	
-	slice = sliceminmax(slice)//function
+	var tableau = [7]int{3, 4, 8, 9, 5, 2, 7} //array
 
-	fmt.Println("New array/slice:",slice)
-	
+	var slice []int = tableau[:] //slice
+
+	slice = sliceminmax(slice) //function
+
+	fmt.Println("New array/slice:", slice)
+
 }
